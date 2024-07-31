3fc43d5536d44c619f62d0d3f2bc4d85fd2cc1c5
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-07-31 10:51:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <PathfindingVisualizer></PathfindingVisualizer>
+      <footer className="footer">
+        <div className="footer-content">
+          <p>&copy; 2024 Ayush Jaiswal. All rights reserved.</p>
+          <p >
+            <a className='social' href="">Email</a> | 
+            <a className='social' href="">Github</a> | 
+            <a className='social' href="">LinkedIn</a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
